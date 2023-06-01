65825929aac60284a0381390c6dcd1559be3bd3a
AlexPst <alexandre-proust.dev@proton.me>
2023-06-01 14:49:00 +0200

    feat: add user detail

--- mq-front/script.js
+++ mq-front/script.js
@@ -1,11 +1,3 @@
-function changeResNumber() {
-    let nbrRes = document.getElementsByClassName('userItem').length;
-    let stringRes = document.getElementById('res');
-    if (nbrRes > 0) {
-        stringRes.innerHTML = nbrRes.toString() + " résultats";
-    }
-}
-
 document.addEventListener('DOMContentLoaded', () => {
   fetch('http://192.168.20.47:8080/api/v1/users')
   .then(response => response.json())
@@ -48,7 +40,34 @@ document.addEventListener('DOMContentLoaded', () => {
         userItem.appendChild(userContainer);
         userList.appendChild(userItem);
     });
-      changeResNumber()
+    document.getElementById('userDetail').style.display = 'none';
+   // Parcours de tous les éléments avec la classe 'userItem'
+  Array.from(userList.children).forEach(userItem => {
+    userItem.addEventListener('click', () => {
+      // Récupération des informations de l'utilisateur correspondant à l'userItem cliqué
+      const name = userItem.querySelector('.userInfos span:nth-child(1)').textContent;
+      const firstName = userItem.querySelector('.userInfos span:nth-child(2)').textContent;
+      const birthDate = userItem.querySelector('.userInfos span:nth-child(3)').textContent;
+      const address = userItem.querySelector('.userInfos span:nth-child(4)').textContent;
+
+      // Récupération de la ville et du code postal
+      const addressParts = address.split(', ');
+      const zipCode = addressParts[1];
+      const city = addressParts[2];
+
+      // Remplissage des informations dans la section 'userDetail'
+      document.getElementById('detailName').textContent = name;
+      document.getElementById('detailFirstName').textContent = firstName;
+      document.getElementById('detailBirthDate').textContent = birthDate;
+      document.getElementById('detailAdress').textContent = address;
+      document.getElementById('detailZipCode').textContent = zipCode;
+      document.getElementById('detailCity').textContent = city;
+
+      // Affichage de la section 'userDetail'
+      document.querySelector('.userDetail').style.display = 'block';
+    });
+  });
+    changeResNumber();
     })
   .catch(error => {
     console.log('Une erreur s\'est produite lors de la récupération des données :', error);
@@ -56,6 +75,7 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 const searchInput = document.getElementById('searchInput');
+searchInput.addEventListener('input', filterUserList);
 
 function filterUserList() {
   const input = document.getElementById('searchInput');
@@ -67,10 +87,28 @@ function filterUserList() {
     const firstName = userItem.querySelector('.userInfos span:nth-child(2)').textContent.toUpperCase();
 
     if (name.includes(filter) || firstName.includes(filter)) {
-        userItem.style.display = '';
+      userItem.style.display = '';
     } else {
-        userItem.style.display = 'none';
+      userItem.style.display = 'none';
       }
   });
-    
+  changeResNumber();
+}
+
+function changeResNumber() {
+  let userItems = document.getElementsByClassName('userItem');
+  let nbRes = 0;
+  
+  // Parcours de tous les éléments avec la classe 'userItem'
+  for (let i = 0; i < userItems.length; i++) {
+    // Vérification si l'élément n'est pas en display 'none'
+    if (window.getComputedStyle(userItems[i]).display !== 'none') {
+      nbRes++;
+    }
+  }
+  
+  let stringRes = document.getElementById('res');
+  if (nbRes > 0) {
+    stringRes.innerHTML = nbRes.toString() + " résultats";
+  }
 }
